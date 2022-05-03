964815bb7bf1866ca755f9a77099992e1f8d9e0a
NIKHIL SHARMA <73777412+nikhil101001@users.noreply.github.com>
2022-05-03 04:42:41 -0700

    adding responsiveness, square, square root

--- index.js
+++ index.js
@@ -2,15 +2,21 @@
 
 let screen = document.getElementById("screen");
 
-buttons = document.querySelectorAll("button");
+let buttons = document.querySelectorAll("button");
 let screenValue = "";
 for (item of buttons) {
   item.addEventListener("click", (e) => {
-    buttonText = e.target.innerText;
-    // console.log("button text is", buttonText);
+    let buttonText = e.target.innerText;
+    console.log("button text is", buttonText);
     if (buttonText == "X") {
       buttonText = "*";
       screenValue += buttonText;
+      screen.value = screenValue;
+    } else if (buttonText == "x²") {
+      screenValue = screenValue * screenValue;
+      screen.value = screenValue;
+    } else if (buttonText == "√x") {
+      screenValue = Math.sqrt(screenValue);
       screen.value = screenValue;
     } else if (buttonText == "C") {
       screenValue = "";
